350f989205930ba027f096d580c3e449efefda48
b97tsk <b97tsk@gmail.com>
2018-05-20 22:41:51 +0700

    Update congest.go

--- congest.go
+++ congest.go
@@ -1,6 +1,7 @@
 package rx
 
 import (
+	"container/list"
 	"context"
 )
 
@@ -9,10 +10,17 @@ type congestOperator struct {
 }
 
 func (op congestOperator) Call(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+	if op.capacity < 65 {
+		return op.forSmallCapacity(ctx, sink, source)
+	}
+	return op.forLargeCapacity(ctx, sink, source)
+}
+
+func (op congestOperator) forSmallCapacity(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 	done := ctx.Done()
-	c := make(chan Notification, op.capacity)
 
+	c := make(chan Notification, op.capacity)
 	go func() {
 		for {
 			select {
@@ -35,6 +43,65 @@ func (op congestOperator) Call(ctx context.Context, sink Observer, source Observ
 		select {
 		case <-done:
 		case c <- t:
+		}
+	})
+
+	return ctx, cancel
+}
+
+func (op congestOperator) forLargeCapacity(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(ctx)
+	done := ctx.Done()
+
+	c := make(chan *list.Element)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case e := <-c:
+				switch t := e.Value.(Notification); {
+				case t.HasValue:
+					sink(t)
+				default:
+					sink(t)
+					cancel()
+					return
+				}
+			}
+		}
+	}()
+
+	q := make(chan Notification)
+	go func() {
+		var queue list.List
+		for {
+			var (
+				in  <-chan Notification
+				out chan<- *list.Element
+			)
+			length := queue.Len()
+			if length < op.capacity {
+				in = q
+			}
+			if length > 0 {
+				out = c
+			}
+			select {
+			case <-done:
+				return
+			case t := <-in:
+				queue.PushBack(t)
+			case out <- queue.Front():
+				queue.Remove(queue.Front())
+			}
+		}
+	}()
+
+	source.Subscribe(ctx, func(t Notification) {
+		select {
+		case <-done:
+		case q <- t:
 		}
 	})
 
