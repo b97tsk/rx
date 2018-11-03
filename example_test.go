aad8b3827faa02eda5063c191d9547a539ea771d
b97tsk <b97tsk@gmail.com>
2018-11-03 17:06:34 +0800

    Add example_test.go

---
+++ example_test.go
@@ -0,0 +1,45 @@
+package rx_test
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/b97tsk/rx"
+)
+
+func Example() {
+	var operators Operators
+
+	Range(1, 10).Pipe(
+		operators.Filter(
+			func(val interface{}, idx int) bool {
+				return val.(int)%2 == 1
+			},
+		),
+		operators.Map(
+			func(val interface{}, idx int) interface{} {
+				return val.(int) * 2
+			},
+		),
+		operators.Do(
+			func(t Notification) {
+				switch {
+				case t.HasValue:
+					fmt.Println(t.Value)
+				case t.HasError:
+					fmt.Println(t.Value)
+				default:
+					fmt.Println("Complete")
+				}
+			},
+		),
+	).Subscribe(context.Background(), NopObserver)
+
+	// Output:
+	// 2
+	// 6
+	// 10
+	// 14
+	// 18
+	// Complete
+}
