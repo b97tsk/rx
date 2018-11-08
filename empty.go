1de37eb40e8c70e53df82c284717a783df6ae0e8
b97tsk <b97tsk@gmail.com>
2018-11-08 12:53:03 +0800

    Update empty.go & never.go

--- empty.go
+++ empty.go
@@ -4,16 +4,15 @@ import (
 	"context"
 )
 
-type emptyOperator struct{}
-
-func (op emptyOperator) Call(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
-	sink.Complete()
-	return canceledCtx, nothingToDo
-}
+var empty = Observable{}.Lift(
+	func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+		sink.Complete()
+		return canceledCtx, nothingToDo
+	},
+)
 
 // Empty creates an Observable that emits no items to the Observer and
 // immediately emits a Complete notification.
 func Empty() Observable {
-	op := emptyOperator{}
-	return Observable{}.Lift(op.Call)
+	return empty
 }
--- never.go
+++ never.go
@@ -4,14 +4,13 @@ import (
 	"context"
 )
 
-type neverOperator struct{}
-
-func (op neverOperator) Call(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
-	return context.WithCancel(ctx)
-}
+var never = Observable{}.Lift(
+	func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+		return context.WithCancel(ctx)
+	},
+)
 
 // Never creates an Observable that never emits anything.
 func Never() Observable {
-	op := neverOperator{}
-	return Observable{}.Lift(op.Call)
+	return never
 }
