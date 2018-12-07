c2d50b999083607b94beff408782d00a2403020d
b97tsk <b97tsk@gmail.com>
2018-12-07 22:52:03 +0800

    Add DoOnNext & DoOnError & DoOnComplete operators

--- do.go
+++ do.go
@@ -4,7 +4,7 @@ import (
 	"context"
 )
 
-// Do creates an Observable that mirrors the source Observable, but perform
+// Do creates an Observable that mirrors the source Observable, but performs
 // a side effect before each emission.
 func (Operators) Do(sink Observer) OperatorFunc {
 	return func(source Observable) Observable {
@@ -13,6 +13,61 @@ func (Operators) Do(sink Observer) OperatorFunc {
 				return source.Subscribe(ctx, func(t Notification) {
 					sink(t)
 					notify(t)
+				})
+			},
+		)
+	}
+}
+
+// DoOnNext creates an Observable that mirrors the source Observable, but
+// performs a side effect before each value.
+func (Operators) DoOnNext(onNext func(interface{})) OperatorFunc {
+	return func(source Observable) Observable {
+		return source.Lift(
+			func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+				return source.Subscribe(ctx, func(t Notification) {
+					if t.HasValue {
+						onNext(t.Value)
+					}
+					sink(t)
+				})
+			},
+		)
+	}
+}
+
+// DoOnError creates an Observable that mirrors the source Observable, in
+// the case that the source emits an Error emission, performs a side effect
+// before mirroring that emission.
+func (Operators) DoOnError(onError func(error)) OperatorFunc {
+	return func(source Observable) Observable {
+		return source.Lift(
+			func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+				return source.Subscribe(ctx, func(t Notification) {
+					if t.HasError {
+						onError(t.Value.(error))
+					}
+					sink(t)
+				})
+			},
+		)
+	}
+}
+
+// DoOnComplete creates an Observable that mirrors the source Observable, in
+// the case that the source emits an Error emission, performs a side effect
+// before mirroring that emission.
+func (Operators) DoOnComplete(onComplete func()) OperatorFunc {
+	return func(source Observable) Observable {
+		return source.Lift(
+			func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
+				return source.Subscribe(ctx, func(t Notification) {
+					if t.HasValue || t.HasError {
+						sink(t)
+						return
+					}
+					onComplete()
+					sink(t)
 				})
 			},
 		)
---
+++ do_test.go
@@ -0,0 +1,91 @@
+package rx_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rx"
+)
+
+func TestOperators_Do(t *testing.T) {
+	n := 0
+	op := operators.Do(func(Notification) { n++ })
+	obs := Defer(func() Observable { return Just(n) })
+	subscribe(
+		t,
+		[]Observable{
+			Concat(Empty().Pipe(op), obs),
+			Concat(Just("A").Pipe(op), obs),
+			Concat(Just("A", "B").Pipe(op), obs),
+			Concat(Concat(Just("A", "B"), Throw(xErrTest)).Pipe(op), obs),
+			obs,
+		},
+		1, xComplete,
+		"A", 3, xComplete,
+		"A", "B", 6, xComplete,
+		"A", "B", xErrTest,
+		9, xComplete,
+	)
+}
+
+func TestOperators_DoOnNext(t *testing.T) {
+	n := 0
+	op := operators.DoOnNext(func(interface{}) { n++ })
+	obs := Defer(func() Observable { return Just(n) })
+	subscribe(
+		t,
+		[]Observable{
+			Concat(Empty().Pipe(op), obs),
+			Concat(Just("A").Pipe(op), obs),
+			Concat(Just("A", "B").Pipe(op), obs),
+			Concat(Concat(Just("A", "B"), Throw(xErrTest)).Pipe(op), obs),
+			obs,
+		},
+		0, xComplete,
+		"A", 1, xComplete,
+		"A", "B", 3, xComplete,
+		"A", "B", xErrTest,
+		5, xComplete,
+	)
+}
+
+func TestOperators_DoOnError(t *testing.T) {
+	n := 0
+	op := operators.DoOnError(func(error) { n++ })
+	obs := Defer(func() Observable { return Just(n) })
+	subscribe(
+		t,
+		[]Observable{
+			Concat(Empty().Pipe(op), obs),
+			Concat(Just("A").Pipe(op), obs),
+			Concat(Just("A", "B").Pipe(op), obs),
+			Concat(Concat(Just("A", "B"), Throw(xErrTest)).Pipe(op), obs),
+			obs,
+		},
+		0, xComplete,
+		"A", 0, xComplete,
+		"A", "B", 0, xComplete,
+		"A", "B", xErrTest,
+		1, xComplete,
+	)
+}
+
+func TestOperators_DoOnComplete(t *testing.T) {
+	n := 0
+	op := operators.DoOnComplete(func() { n++ })
+	obs := Defer(func() Observable { return Just(n) })
+	subscribe(
+		t,
+		[]Observable{
+			Concat(Empty().Pipe(op), obs),
+			Concat(Just("A").Pipe(op), obs),
+			Concat(Just("A", "B").Pipe(op), obs),
+			Concat(Concat(Just("A", "B"), Throw(xErrTest)).Pipe(op), obs),
+			obs,
+		},
+		1, xComplete,
+		"A", 2, xComplete,
+		"A", "B", 3, xComplete,
+		"A", "B", xErrTest,
+		3, xComplete,
+	)
+}
