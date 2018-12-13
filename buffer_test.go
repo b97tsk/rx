03062b3daeac58a6611746a7b23a66fe24889fb4
b97tsk <b97tsk@gmail.com>
2018-12-13 23:31:47 +0800

    Update buffer_test.go

--- buffer_test.go
+++ buffer_test.go
@@ -7,22 +7,33 @@ import (
 )
 
 func TestOperators_Buffer(t *testing.T) {
-	addLatency := addLatencyToValue(1, 2)
 	subscribe(
 		t,
 		[]Observable{
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
-				addLatency,
+				addLatencyToValue(1, 2),
 				operators.Buffer(Interval(step(2))),
 				toString,
 			),
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
-				addLatency,
+				addLatencyToValue(1, 2),
 				operators.Buffer(Interval(step(4))),
+				toString,
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(1, 2),
+				operators.Buffer(Interval(step(6))),
+				toString,
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(1, 2),
+				operators.Buffer(Interval(step(8))),
 				toString,
 			),
 		},
 		"[A]", "[B]", "[C]", "[D]", "[E]", "[F]", xComplete,
 		"[A B]", "[C D]", "[E F]", xComplete,
+		"[A B C]", "[D E F]", xComplete,
+		"[A B C D]", xComplete,
 	)
 }
