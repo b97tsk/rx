e7fa12ee1a0da5232d8a276987e1fabdcea84a42
b97tsk <b97tsk@gmail.com>
2018-12-14 23:23:35 +0800

    Update Multicast operator

--- multicast.go
+++ multicast.go
@@ -4,20 +4,18 @@ import (
 	"context"
 )
 
-// Multicast creates an Observable that calls subjectFactory function to create
-// a Subject, passes this Subject to selector function, where you have a chance
-// to make more subscriptions to that Subject, then subscribes the source
-// Observable to emit values to that Subject. Moreover, the selector function
-// returns an Observable which is subscribed to the sink Observer; if it
-// completes or emits an error, all subscriptions must be canceled.
-func (Operators) Multicast(subjectFactory func() Subject, selector func(context.Context, Subject) Observable) OperatorFunc {
+// Multicast creates an Observable that calls the subjectFactory function
+// to create a Subject, subscribes the Subject to emit values to the sink
+// Observer, then subscribes the source Observable to emit values to the
+// Subject. In the subjectFactory function, you can create subscriptions to
+// the Subject as many as you need, just before it returns.
+func (Operators) Multicast(subjectFactory func(context.Context) Subject) OperatorFunc {
 	return func(source Observable) Observable {
 		return source.Lift(
 			func(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
 				ctx, cancel := context.WithCancel(ctx)
-				subject := subjectFactory()
-				obs := selector(ctx, subject)
-				obs.Subscribe(ctx, Finally(sink, cancel))
+				subject := subjectFactory(ctx)
+				subject.Subscribe(ctx, Finally(sink, cancel))
 				if isDone(ctx) {
 					return canceledCtx, nothingToDo
 				}
--- multicast_test.go
+++ multicast_test.go
@@ -13,19 +13,22 @@ func TestOperators_Multicast(t *testing.T) {
 		[]Observable{
 			Interval(step(1)).Pipe(
 				operators.Multicast(
-					NewSubject,
-					func(ctx context.Context, subject Subject) Observable {
-						return Zip(
-							subject.Pipe(operators.Take(4)),
-							subject.Pipe(operators.Skip(4), operators.Take(4)),
-						).Pipe(
-							operators.Map(
-								func(val interface{}, idx int) interface{} {
-									vals := val.([]interface{})
-									return vals[0].(int) * vals[1].(int)
-								},
+					func(ctx context.Context) Subject {
+						subject := NewSubject()
+						return Subject{
+							Zip(
+								subject.Pipe(operators.Take(4)),
+								subject.Pipe(operators.Skip(4), operators.Take(4)),
+							).Pipe(
+								operators.Map(
+									func(val interface{}, idx int) interface{} {
+										vals := val.([]interface{})
+										return vals[0].(int) * vals[1].(int)
+									},
+								),
 							),
-						)
+							subject.Observer,
+						}
 					},
 				),
 			),
