7257c083562c8fc9da2194d6cac317e9f2bab029
b97tsk <b97tsk@gmail.com>
2019-06-30 08:22:57 +0800

    Update delayWhen.go

--- delayWhen.go
+++ delayWhen.go
@@ -2,8 +2,6 @@ package rx
 
 import (
 	"context"
-
-	"github.com/b97tsk/rx/x/atomic"
 )
 
 type delayWhenOperator struct {
@@ -13,53 +11,72 @@ type delayWhenOperator struct {
 func (op delayWhenOperator) Call(ctx context.Context, sink Observer, source Observable) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 
-	sink = Mutex(Finally(sink, cancel))
+	sink = Finally(sink, cancel)
 
-	var (
-		outerIndex  = -1
-		activeCount = atomic.Uint32(1)
-	)
+	type X struct {
+		Index       int
+		ActiveCount int
+	}
+	cx := make(chan *X, 1)
+	cx <- &X{ActiveCount: 1}
 
-	doSchedule := func(val interface{}, idx int) {
-		scheduleCtx, scheduleCancel := context.WithCancel(ctx)
-
-		var observer Observer
-		observer = func(t Notification) {
-			observer = NopObserver
-			scheduleCancel()
+	source.Subscribe(ctx, func(t Notification) {
+		if x, ok := <-cx; ok {
 			switch {
 			case t.HasValue:
-				sink.Next(val)
+				outerIndex := x.Index
+				outerValue := t.Value
+				x.Index++
+				x.ActiveCount++
 
-				if activeCount.Sub(1) == 0 {
-					sink.Complete()
+				cx <- x
+
+				scheduleCtx, scheduleCancel := context.WithCancel(ctx)
+
+				var observer Observer
+				observer = func(t Notification) {
+					observer = NopObserver
+					scheduleCancel()
+					if x, ok := <-cx; ok {
+						x.ActiveCount--
+						switch {
+						case t.HasValue:
+							sink.Next(outerValue)
+							if x.ActiveCount == 0 {
+								close(cx)
+								sink.Complete()
+								return
+							}
+							cx <- x
+						case t.HasError:
+							close(cx)
+							sink(t)
+						default:
+							if x.ActiveCount == 0 {
+								close(cx)
+								sink(t)
+								return
+							}
+							cx <- x
+						}
+					}
 				}
 
+				obs := op.DurationSelector(outerValue, outerIndex)
+				obs.Subscribe(scheduleCtx, observer.Notify)
+
 			case t.HasError:
+				close(cx)
 				sink(t)
 
 			default:
-				if activeCount.Sub(1) == 0 {
+				x.ActiveCount--
+				if x.ActiveCount == 0 {
+					close(cx)
 					sink(t)
+					return
 				}
-			}
-		}
-
-		obs := op.DurationSelector(val, idx)
-		obs.Subscribe(scheduleCtx, observer.Notify)
-	}
-
-	source.Subscribe(ctx, func(t Notification) {
-		switch {
-		case t.HasValue:
-			outerIndex++
-			activeCount.Add(1)
-			doSchedule(t.Value, outerIndex)
-		case t.HasError:
-			sink(t)
-		default:
-			if activeCount.Sub(1) == 0 {
-				sink(t)
+				cx <- x
 			}
 		}
 	})
