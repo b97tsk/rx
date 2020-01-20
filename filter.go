3307eb5c23af2f2ff0e015c3b0babb09d3757770
b97tsk <b97tsk@gmail.com>
2020-01-20 12:32:00 +0800

    Move Exclude next to Filter

--- filter.go
+++ filter.go
@@ -24,3 +24,24 @@ func (Operators) Filter(predicate func(interface{}, int) bool) Operator {
 		}
 	}
 }
+
+// Exclude creates an Observable that filter items emitted by the source
+// Observable by only emitting those that do not satisfy a specified predicate.
+func (Operators) Exclude(predicate func(interface{}, int) bool) Operator {
+	return func(source Observable) Observable {
+		return func(ctx context.Context, sink Observer) (context.Context, context.CancelFunc) {
+			var sourceIndex = -1
+			return source.Subscribe(ctx, func(t Notification) {
+				switch {
+				case t.HasValue:
+					sourceIndex++
+					if !predicate(t.Value, sourceIndex) {
+						sink(t)
+					}
+				default:
+					sink(t)
+				}
+			})
+		}
+	}
+}
--- filter_test.go
+++ filter_test.go
@@ -24,3 +24,22 @@ func TestOperators_Filter(t *testing.T) {
 		1, 2, 3, 4, xErrTest,
 	)
 }
+
+func TestOperators_Exclude(t *testing.T) {
+	excludeLessThan5 := operators.Exclude(
+		func(val interface{}, idx int) bool {
+			return val.(int) < 5
+		},
+	)
+	subscribe(
+		t,
+		[]Observable{
+			Just(1, 2, 3, 4, 5, 4, 3, 2, 1).Pipe(excludeLessThan5),
+			Range(1, 9).Pipe(excludeLessThan5),
+			Concat(Range(1, 9), Throw(xErrTest)).Pipe(excludeLessThan5),
+		},
+		5, xComplete,
+		5, 6, 7, 8, xComplete,
+		5, 6, 7, 8, xErrTest,
+	)
+}
