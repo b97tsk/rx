3ca06348a14dcc8c556127baaf6ac19d1c48634f
b97tsk <b97tsk@gmail.com>
2020-03-26 22:29:49 +0800

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -10,7 +10,8 @@ import (
 func Example() {
 	var operators rx.Operators
 
-	ctx, _ := rx.Range(1, 10).Pipe(
+	// Create an observable and apply some operators.
+	obs := rx.Range(1, 10).Pipe(
 		operators.Filter(
 			func(val interface{}, idx int) bool {
 				return val.(int)%2 == 1
@@ -33,13 +34,32 @@ func Example() {
 				}
 			},
 		),
-	).Subscribe(
-		context.Background(),
-		rx.NopObserver,
 	)
 
+	// To Subscribe to an observable, you call its Subscribe method with
+	// a context and an observer as parameters, and you get another context
+	// and a cancel function in return.
+	ctx, cancel := obs.Subscribe(context.Background(), rx.NopObserver)
+
+	// The returned context will be cancelled when the subscription completes.
 	// Since this example has no goroutines involved, it must have already done.
 	<-ctx.Done() // Wait for it done, though it's not necessary.
+
+	// Subscriptions to observables are cancellable, but since this example has
+	// already finished its work, there is nothing left to cancel.
+	cancel() // This is a noop in this example.
+
+	// To check if a subscription to an observable is really complete without
+	// an error, we can call the Err() method of the returned context to get
+	// an error and check if it equals to rx.Complete.
+	switch ctx.Err() {
+	case rx.Complete:
+		fmt.Println("No Error")
+	case context.Canceled:
+		fmt.Println("Canceled")
+	default:
+		fmt.Println(ctx.Err())
+	}
 
 	// Output:
 	// 2
@@ -48,4 +68,5 @@ func Example() {
 	// 14
 	// 18
 	// Complete
+	// No Error
 }
