clfsoft/rxgo

Avoid recursive calls

observable/avoidRecursiveCalls.go
@@ -1,0 +1,21 @@
+package observable
+
+import (
+	"math"
+	"sync/atomic"
+)
+
+type avoidRecursiveCalls struct {
+	n uint32
+}
+
+func (arc *avoidRecursiveCalls) Do(work func()) {
+	if atomic.AddUint32(&arc.n, 1) > 1 {
+		return
+	}
+	yes := true
+	for yes {
+		work()
+		yes = atomic.AddUint32(&arc.n, math.MaxUint32) > 0
+	}
+}

b97tsk/rx

avoidRecursiveCalls.go
@@ -1,0 +1,21 @@
+package rx
+
+import (
+	"math"
+	"sync/atomic"
+)
+
+type avoidRecursiveCalls struct {
+	n uint32
+}
+
+func (arc *avoidRecursiveCalls) Do(work func()) {
+	if atomic.AddUint32(&arc.n, 1) > 1 {
+		return
+	}
+	yes := true
+	for yes {
+		work()
+		yes = atomic.AddUint32(&arc.n, math.MaxUint32) > 0
+	}
+}