clfsoft/rxgo

Rename exported Operator type

observable/bufferCount_test.go
@@ -12,9 +12,9 @@ func TestOperators_BufferCount(t *testing.T) {
 		[]Observable{
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(operators.BufferCount(2), toString),
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(operators.BufferCount(3), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 1}.MakeFunc(), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 2}.MakeFunc(), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 4}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfig{3, 1}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfig{3, 2}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfig{3, 4}.MakeFunc(), toString),
 		},
 		"[A B]", "[C D]", "[E F]", "[G]", xComplete,
 		"[A B C]", "[D E F]", "[G]", xComplete,

observable/congestingMerge_test.go
@@ -19,7 +19,7 @@ func TestOperators_CongestingMergeAll(t *testing.T) {
 				Just("A", "B").Pipe(addLatencyToValue(3, 5)),
 				Just("C", "D").Pipe(addLatencyToValue(2, 4)),
 				Just("E", "F").Pipe(addLatencyToValue(1, 3)),
-			).Pipe(CongestingMergeOperator{ProjectToObservable, 1}.MakeFunc()),
+			).Pipe(CongestingMergeConfig{ProjectToObservable, 1}.MakeFunc()),
 		},
 		"E", "C", "A", "F", "D", "B", xComplete,
 		"A", "B", "C", "D", "E", "F", xComplete,

b97tsk/rx

bufferCount_test.go
@@ -12,9 +12,9 @@ func TestOperators_BufferCount(t *testing.T) {
 		[]Observable{
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(operators.BufferCount(2), toString),
 			Just("A", "B", "C", "D", "E", "F", "G").Pipe(operators.BufferCount(3), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 1}.MakeFunc(), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 2}.MakeFunc(), toString),
-			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountOperator{3, 4}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfigure{3, 1}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfigure{3, 2}.MakeFunc(), toString),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(BufferCountConfigure{3, 4}.MakeFunc(), toString),
 		},
 		"[A B]", "[C D]", "[E F]", "[G]", xComplete,
 		"[A B C]", "[D E F]", "[G]", xComplete,

congestingMerge_test.go
@@ -19,7 +19,7 @@ func TestOperators_CongestingMergeAll(t *testing.T) {
 				Just("A", "B").Pipe(addLatencyToValue(3, 5)),
 				Just("C", "D").Pipe(addLatencyToValue(2, 4)),
 				Just("E", "F").Pipe(addLatencyToValue(1, 3)),
-			).Pipe(CongestingMergeOperator{ProjectToObservable, 1}.MakeFunc()),
+			).Pipe(CongestingMergeConfigure{ProjectToObservable, 1}.MakeFunc()),
 		},
 		"E", "C", "A", "F", "D", "B", xComplete,
 		"A", "B", "C", "D", "E", "F", xComplete,