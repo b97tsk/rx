clfsoft/rxgo

Add simple tests for coverage

observable/reduce_test.go
@@ -1,0 +1,40 @@
+package observable_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rxgo/observable"
+)
+
+func TestOperators_Reduce(t *testing.T) {
+	max := func(seed, val interface{}, idx int) interface{} {
+		if seed.(int) > val.(int) {
+			return seed
+		}
+		return val
+	}
+
+	sum := func(seed, val interface{}, idx int) interface{} {
+		return seed.(int) + val.(int)
+	}
+
+	subscribe(
+		t,
+		[]Observable{
+			Range(1, 7).Pipe(operators.Reduce(max)),
+			Just(42).Pipe(operators.Reduce(max)),
+			Empty().Pipe(operators.Reduce(max)),
+			Range(1, 7).Pipe(operators.Reduce(sum)),
+			Just(42).Pipe(operators.Reduce(sum)),
+			Empty().Pipe(operators.Reduce(sum)),
+			Throw(xErrTest).Pipe(operators.Reduce(sum)),
+		},
+		6, xComplete,
+		42, xComplete,
+		xComplete,
+		21, xComplete,
+		42, xComplete,
+		xComplete,
+		xErrTest,
+	)
+}

observable/skipWhile_test.go
@@ -1,0 +1,26 @@
+package observable_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rxgo/observable"
+)
+
+func TestOperators_SkipWhile(t *testing.T) {
+	skipLessThan5 := operators.SkipWhile(
+		func(val interface{}, idx int) bool {
+			return val.(int) < 5
+		},
+	)
+	subscribe(
+		t,
+		[]Observable{
+			Just(1, 2, 3, 4, 5, 4, 3, 2, 1).Pipe(skipLessThan5),
+			Concat(Range(1, 9), Throw(xErrTest)).Pipe(skipLessThan5),
+			Concat(Range(1, 5), Throw(xErrTest)).Pipe(skipLessThan5),
+		},
+		5, 4, 3, 2, 1, xComplete,
+		5, 6, 7, 8, xErrTest,
+		xErrTest,
+	)
+}

observable/subject_test.go
@@ -1,0 +1,56 @@
+package observable_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/b97tsk/rxgo/observable"
+)
+
+func TestSubject(t *testing.T) {
+	sum := func(acc, val interface{}, idx int) interface{} {
+		return acc.(int) + val.(int)
+	}
+
+	t.Run("Complete", func(t *testing.T) {
+		subject := NewSubject()
+
+		Just(3, 4, 5).Pipe(
+			addLatencyToValue(1, 1),
+		).Subscribe(context.Background(), subject.Observer)
+
+		subscribe(
+			t,
+			[]Observable{
+				Zip(
+					subject.Observable,
+					subject.Pipe(operators.Scan(sum)),
+				).Pipe(toString),
+			},
+			"[3 3]", "[4 7]", "[5 12]", xComplete,
+		)
+
+		subscribe(t, []Observable{subject.Observable}, xComplete)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		subject := NewSubject()
+
+		Concat(Just(3, 4, 5), Throw(xErrTest)).Pipe(
+			addLatencyToNotification(1, 1),
+		).Subscribe(context.Background(), subject.Observer)
+
+		subscribe(
+			t,
+			[]Observable{
+				Zip(
+					subject.Observable,
+					subject.Pipe(operators.Scan(sum)),
+				).Pipe(toString),
+			},
+			"[3 3]", "[4 7]", "[5 12]", xErrTest,
+		)
+
+		subscribe(t, []Observable{subject.Observable}, xErrTest)
+	})
+}

observable/throttleTime_test.go
@@ -1,0 +1,38 @@
+package observable_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rxgo/observable"
+)
+
+func TestOperators_ThrottleTime(t *testing.T) {
+	subscribe(
+		t,
+		[]Observable{
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 2),
+				operators.ThrottleTime(step(3)),
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 4),
+				ThrottleTimeOperator{
+					Duration: step(9),
+					Leading:  false,
+					Trailing: true,
+				}.MakeFunc(),
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 4),
+				ThrottleTimeOperator{
+					Duration: step(9),
+					Leading:  true,
+					Trailing: true,
+				}.MakeFunc(),
+			),
+		},
+		"A", "C", "E", "G", xComplete,
+		"C", "E", "G", xComplete,
+		"A", "C", "E", "G", xComplete,
+	)
+}

b97tsk/rx

reduce_test.go
@@ -1,0 +1,40 @@
+package rx_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rx"
+)
+
+func TestOperators_Reduce(t *testing.T) {
+	max := func(seed, val interface{}, idx int) interface{} {
+		if seed.(int) > val.(int) {
+			return seed
+		}
+		return val
+	}
+
+	sum := func(seed, val interface{}, idx int) interface{} {
+		return seed.(int) + val.(int)
+	}
+
+	subscribe(
+		t,
+		[]Observable{
+			Range(1, 7).Pipe(operators.Reduce(max)),
+			Just(42).Pipe(operators.Reduce(max)),
+			Empty().Pipe(operators.Reduce(max)),
+			Range(1, 7).Pipe(operators.Reduce(sum)),
+			Just(42).Pipe(operators.Reduce(sum)),
+			Empty().Pipe(operators.Reduce(sum)),
+			Throw(xErrTest).Pipe(operators.Reduce(sum)),
+		},
+		6, xComplete,
+		42, xComplete,
+		xComplete,
+		21, xComplete,
+		42, xComplete,
+		xComplete,
+		xErrTest,
+	)
+}

skipWhile_test.go
@@ -1,0 +1,26 @@
+package rx_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rx"
+)
+
+func TestOperators_SkipWhile(t *testing.T) {
+	skipLessThan5 := operators.SkipWhile(
+		func(val interface{}, idx int) bool {
+			return val.(int) < 5
+		},
+	)
+	subscribe(
+		t,
+		[]Observable{
+			Just(1, 2, 3, 4, 5, 4, 3, 2, 1).Pipe(skipLessThan5),
+			Concat(Range(1, 9), Throw(xErrTest)).Pipe(skipLessThan5),
+			Concat(Range(1, 5), Throw(xErrTest)).Pipe(skipLessThan5),
+		},
+		5, 4, 3, 2, 1, xComplete,
+		5, 6, 7, 8, xErrTest,
+		xErrTest,
+	)
+}

subject_test.go
@@ -1,0 +1,56 @@
+package rx_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/b97tsk/rx"
+)
+
+func TestSubject(t *testing.T) {
+	sum := func(acc, val interface{}, idx int) interface{} {
+		return acc.(int) + val.(int)
+	}
+
+	t.Run("Complete", func(t *testing.T) {
+		subject := NewSubject()
+
+		Just(3, 4, 5).Pipe(
+			addLatencyToValue(1, 1),
+		).Subscribe(context.Background(), subject.Observer)
+
+		subscribe(
+			t,
+			[]Observable{
+				Zip(
+					subject.Observable,
+					subject.Pipe(operators.Scan(sum)),
+				).Pipe(toString),
+			},
+			"[3 3]", "[4 7]", "[5 12]", xComplete,
+		)
+
+		subscribe(t, []Observable{subject.Observable}, xComplete)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		subject := NewSubject()
+
+		Concat(Just(3, 4, 5), Throw(xErrTest)).Pipe(
+			addLatencyToNotification(1, 1),
+		).Subscribe(context.Background(), subject.Observer)
+
+		subscribe(
+			t,
+			[]Observable{
+				Zip(
+					subject.Observable,
+					subject.Pipe(operators.Scan(sum)),
+				).Pipe(toString),
+			},
+			"[3 3]", "[4 7]", "[5 12]", xErrTest,
+		)
+
+		subscribe(t, []Observable{subject.Observable}, xErrTest)
+	})
+}

throttleTime_test.go
@@ -1,0 +1,38 @@
+package rx_test
+
+import (
+	"testing"
+
+	. "github.com/b97tsk/rx"
+)
+
+func TestOperators_ThrottleTime(t *testing.T) {
+	subscribe(
+		t,
+		[]Observable{
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 2),
+				operators.ThrottleTime(step(3)),
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 4),
+				ThrottleTimeOperator{
+					Duration: step(9),
+					Leading:  false,
+					Trailing: true,
+				}.MakeFunc(),
+			),
+			Just("A", "B", "C", "D", "E", "F", "G").Pipe(
+				addLatencyToValue(0, 4),
+				ThrottleTimeOperator{
+					Duration: step(9),
+					Leading:  true,
+					Trailing: true,
+				}.MakeFunc(),
+			),
+		},
+		"A", "C", "E", "G", xComplete,
+		"C", "E", "G", xComplete,
+		"A", "C", "E", "G", xComplete,
+	)
+}