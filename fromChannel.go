7622c0038e60995fb712cfb2c235ba728a2d458c
b97tsk <b97tsk@gmail.com>
2020-04-06 14:55:02 +0800

    Update fromChannel.go

--- fromChannel.go
+++ fromChannel.go
@@ -4,33 +4,29 @@ import (
 	"context"
 )
 
-type fromChannelObservable struct {
-	Chan <-chan interface{}
-}
-
-func (obs fromChannelObservable) Subscribe(ctx context.Context, sink Observer) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case val, ok := <-obs.Chan:
-			if !ok {
-				sink.Complete()
-				return
-			}
-			sink.Next(val)
-			// Check if ctx is canceled before next loop, such that
-			// Take(1) would exactly take one from the channel.
-			if ctx.Err() != nil {
-				return
-			}
-		}
-	}
-}
-
 // FromChannel creates an Observable that emits values from a channel, and
 // completes when the channel closes.
 func FromChannel(c <-chan interface{}) Observable {
-	obs := fromChannelObservable{c}
-	return Create(obs.Subscribe)
+	return Create(
+		func(ctx context.Context, sink Observer) {
+			done := ctx.Done()
+			for {
+				select {
+				case <-done:
+					return
+				case val, ok := <-c:
+					if !ok {
+						sink.Complete()
+						return
+					}
+					sink.Next(val)
+					// Check if ctx is canceled before next loop, such that
+					// Take(1) would exactly take one from the channel.
+					if ctx.Err() != nil {
+						return
+					}
+				}
+			}
+		},
+	)
 }
