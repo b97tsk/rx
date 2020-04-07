4a28f1a91c5fa173c1e0efa82dfe7fb1e64a5b56
b97tsk <b97tsk@gmail.com>
2020-04-07 17:11:04 +0800

    Update concat.go & onErrorResumeNext.go

--- concat.go
+++ concat.go
@@ -4,82 +4,7 @@ import (
 	"context"
 
 	"github.com/b97tsk/rx/x/misc"
-	"github.com/b97tsk/rx/x/queue"
 )
-
-type concatObservable struct {
-	Source  Observable
-	Project func(interface{}, int) Observable
-}
-
-func (obs concatObservable) Subscribe(ctx context.Context, sink Observer) {
-	sink = Mutex(sink)
-
-	type X struct {
-		Index       int
-		ActiveCount int
-		Buffer      queue.Queue
-	}
-	cx := make(chan *X, 1)
-	cx <- &X{ActiveCount: 1}
-
-	var doNextLocked func(*X)
-
-	doNextLocked = func(x *X) {
-		var avoidRecursive misc.AvoidRecursive
-		avoidRecursive.Do(func() {
-			if x.Buffer.Len() == 0 {
-				x.ActiveCount--
-				if x.ActiveCount == 0 {
-					sink.Complete()
-				}
-				return
-			}
-
-			sourceIndex := x.Index
-			sourceValue := x.Buffer.PopFront()
-			x.Index++
-
-			obs := obs.Project(sourceValue, sourceIndex)
-			obs.Subscribe(ctx, func(t Notification) {
-				switch {
-				case t.HasValue || t.HasError:
-					sink(t)
-				default:
-					avoidRecursive.Do(func() {
-						x := <-cx
-						doNextLocked(x)
-						cx <- x
-					})
-				}
-			})
-		})
-	}
-
-	obs.Source.Subscribe(ctx, func(t Notification) {
-		switch {
-		case t.HasValue:
-			x := <-cx
-			x.Buffer.PushBack(t.Value)
-			if x.ActiveCount == 1 {
-				x.ActiveCount++
-				doNextLocked(x)
-			}
-			cx <- x
-
-		case t.HasError:
-			sink(t)
-
-		default:
-			x := <-cx
-			x.ActiveCount--
-			if x.ActiveCount == 0 {
-				sink(t)
-			}
-			cx <- x
-		}
-	})
-}
 
 // Concat creates an output Observable which sequentially emits all values
 // from given Observable and then moves on to the next.
@@ -90,9 +15,33 @@ func Concat(observables ...Observable) Observable {
 	if len(observables) == 0 {
 		return Empty()
 	}
-	obs := concatObservable{
-		FromObservables(observables...),
-		ProjectToObservable,
-	}
-	return Create(obs.Subscribe)
+	return Create(
+		func(ctx context.Context, sink Observer) {
+			var (
+				observer       Observer
+				avoidRecursive misc.AvoidRecursive
+			)
+
+			remainder := observables
+			subscribe := func() {
+				source := remainder[0]
+				remainder = remainder[1:]
+				source.Subscribe(ctx, observer)
+			}
+
+			observer = func(t Notification) {
+				if t.HasValue || t.HasError {
+					sink(t)
+					return
+				}
+				if len(remainder) == 0 {
+					sink.Complete()
+					return
+				}
+				avoidRecursive.Do(subscribe)
+			}
+
+			avoidRecursive.Do(subscribe)
+		},
+	)
 }
--- onErrorResumeNext.go
+++ onErrorResumeNext.go
@@ -6,44 +6,39 @@ import (
 	"github.com/b97tsk/rx/x/misc"
 )
 
-type onErrorResumeNextObservable struct {
-	Observables []Observable
-}
-
-func (obs onErrorResumeNextObservable) Subscribe(ctx context.Context, sink Observer) {
-	var (
-		observer       Observer
-		avoidRecursive misc.AvoidRecursive
-	)
-
-	remainder := obs.Observables
-	subscribe := func() {
-		source := remainder[0]
-		remainder = remainder[1:]
-		source.Subscribe(ctx, observer)
-	}
-
-	observer = func(t Notification) {
-		if t.HasValue {
-			sink(t)
-			return
-		}
-		if len(remainder) == 0 {
-			sink.Complete()
-			return
-		}
-		avoidRecursive.Do(subscribe)
-	}
-
-	avoidRecursive.Do(subscribe)
-}
-
 // OnErrorResumeNext creates an Observable that concatenates the provided
 // Observables. It's like Concat, but errors are ignored.
 func OnErrorResumeNext(observables ...Observable) Observable {
 	if len(observables) == 0 {
 		return Empty()
 	}
-	obs := onErrorResumeNextObservable{observables}
-	return Create(obs.Subscribe)
+	return Create(
+		func(ctx context.Context, sink Observer) {
+			var (
+				observer       Observer
+				avoidRecursive misc.AvoidRecursive
+			)
+
+			remainder := observables
+			subscribe := func() {
+				source := remainder[0]
+				remainder = remainder[1:]
+				source.Subscribe(ctx, observer)
+			}
+
+			observer = func(t Notification) {
+				if t.HasValue {
+					sink(t)
+					return
+				}
+				if len(remainder) == 0 {
+					sink.Complete()
+					return
+				}
+				avoidRecursive.Do(subscribe)
+			}
+
+			avoidRecursive.Do(subscribe)
+		},
+	)
 }
