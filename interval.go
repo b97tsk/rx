b5b48b9984635d924dea9653afd828101d265b09
b97tsk <b97tsk@gmail.com>
2020-05-24 17:50:42 +0800

    Update Timer function

--- interval.go
+++ interval.go
@@ -10,29 +10,37 @@ import (
 type intervalObservable struct {
 	InitialDelay time.Duration
 	Period       time.Duration
+	HasPeriod    bool
 }
 
 func (obs intervalObservable) Subscribe(ctx context.Context, sink Observer) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 
-	if obs.InitialDelay != obs.Period {
-		schedule.Once(ctx, obs.InitialDelay, func() {
+	if obs.HasPeriod {
+		if obs.InitialDelay != obs.Period {
+			schedule.Once(ctx, obs.InitialDelay, func() {
+				index := 0
+				wait := make(chan struct{})
+				schedule.Periodic(ctx, obs.Period, func() {
+					<-wait
+					sink.Next(index)
+					index++
+				})
+				sink.Next(index)
+				index++
+				close(wait)
+			})
+		} else {
 			index := 0
-			wait := make(chan struct{})
 			schedule.Periodic(ctx, obs.Period, func() {
-				<-wait
 				sink.Next(index)
 				index++
 			})
-			sink.Next(index)
-			index++
-			close(wait)
-		})
+		}
 	} else {
-		index := 0
-		schedule.Periodic(ctx, obs.Period, func() {
-			sink.Next(index)
-			index++
+		schedule.Once(ctx, obs.InitialDelay, func() {
+			sink.Next(0)
+			sink.Complete()
 		})
 	}
 
@@ -42,13 +50,25 @@ func (obs intervalObservable) Subscribe(ctx context.Context, sink Observer) (con
 // Interval creates an Observable that emits sequential integers every
 // specified interval of time.
 func Interval(period time.Duration) Observable {
-	return intervalObservable{period, period}.Subscribe
+	return intervalObservable{period, period, true}.Subscribe
 }
 
 // Timer creates an Observable that starts emitting after an initialDelay and
 // emits ever increasing integers after each period of time thereafter.
 //
 // Its like Interval, but you can specify when should the emissions start.
-func Timer(initialDelay, period time.Duration) Observable {
-	return intervalObservable{initialDelay, period}.Subscribe
+//
+// If period is not specified, the output Observable emits only one value,
+// zero. Otherwise, it emits an infinite sequence.
+func Timer(initialDelay time.Duration, period ...time.Duration) Observable {
+	obs := intervalObservable{InitialDelay: initialDelay}
+	switch len(period) {
+	case 0:
+	case 1:
+		obs.Period = period[0]
+		obs.HasPeriod = true
+	default:
+		panic("too many parameters")
+	}
+	return obs.Subscribe
 }
--- interval_test.go
+++ interval_test.go
@@ -9,21 +9,32 @@ import (
 )
 
 func TestInterval(t *testing.T) {
+	Subscribe(
+		t,
+		rx.CombineLatest(
+			rx.Interval(Step(1)).Pipe(operators.Take(3)),
+			rx.Interval(0).Pipe(operators.Take(3)),
+		).Pipe(ToString()),
+		"[0 2]", "[1 2]", "[2 2]", rx.Complete,
+	)
+}
+
+func TestTimer(t *testing.T) {
 	SubscribeN(
 		t,
 		[]rx.Observable{
 			rx.CombineLatest(
-				rx.Interval(Step(1)).Pipe(operators.Take(3)),
-				rx.Interval(0).Pipe(operators.Take(3)),
-			).Pipe(ToString()),
-			rx.CombineLatest(
 				rx.Interval(Step(2)).Pipe(operators.Take(3)),
 				rx.Timer(Step(1), Step(2)).Pipe(operators.Take(3)),
 			).Pipe(ToString()),
+			rx.CombineLatest(
+				rx.Interval(Step(2)).Pipe(operators.Take(3)),
+				rx.Timer(Step(1)),
+			).Pipe(ToString()),
 		},
 		[][]interface{}{
-			{"[0 2]", "[1 2]", "[2 2]", rx.Complete},
 			{"[0 0]", "[0 1]", "[1 1]", "[1 2]", "[2 2]", rx.Complete},
+			{"[0 0]", "[1 0]", "[2 0]", rx.Complete},
 		},
 	)
 }
