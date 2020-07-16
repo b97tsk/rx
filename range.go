f71b1c14cae2c5b1285f8648c26f75f1e130cd3f
b97tsk <b97tsk@gmail.com>
2020-07-16 12:07:20 +0800

    Update range.go

--- range.go
+++ range.go
@@ -7,9 +7,6 @@ import (
 // Range creates an Observable that emits a sequence of integers within a
 // specified range.
 func Range(low, high int) Observable {
-	if low >= high {
-		return Empty()
-	}
 	return func(ctx context.Context, sink Observer) {
 		for idx := low; idx < high; idx++ {
 			if ctx.Err() != nil {
