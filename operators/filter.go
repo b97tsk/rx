0647083ba38ce51be689003b10e532ae70f39488
b97tsk <b97tsk@gmail.com>
2020-08-11 12:37:15 +0800

    Add FilterMap operator

--- operators/filter.go
+++ operators/filter.go
@@ -27,6 +27,28 @@ func Filter(predicate func(interface{}, int) bool) rx.Operator {
 	}
 }
 
+// FilterMap creates an Observable that passes each item emitted by the source
+// Observable to a specified predicate and emits their mapping, the first
+// return value of the predicate, if the second is true.
+func FilterMap(predicate func(interface{}, int) (interface{}, bool)) rx.Operator {
+	return func(source rx.Observable) rx.Observable {
+		return func(ctx context.Context, sink rx.Observer) {
+			sourceIndex := -1
+			source.Subscribe(ctx, func(t rx.Notification) {
+				switch {
+				case t.HasValue:
+					sourceIndex++
+					if val, ok := predicate(t.Value, sourceIndex); ok {
+						sink.Next(val)
+					}
+				default:
+					sink(t)
+				}
+			})
+		}
+	}
+}
+
 // Exclude creates an Observable that filter items emitted by the source
 // Observable by only emitting those that do not satisfy a specified predicate.
 func Exclude(predicate func(interface{}, int) bool) rx.Operator {
--- operators/filter_test.go
+++ operators/filter_test.go
@@ -29,6 +29,27 @@ func TestFilter(t *testing.T) {
 	)
 }
 
+func TestFilterMap(t *testing.T) {
+	filterLessThan5 := operators.FilterMap(
+		func(val interface{}, idx int) (interface{}, bool) {
+			return val, val.(int) < 5
+		},
+	)
+	SubscribeN(
+		t,
+		[]rx.Observable{
+			rx.Just(1, 2, 3, 4, 5, 4, 3, 2, 1).Pipe(filterLessThan5),
+			rx.Range(1, 9).Pipe(filterLessThan5),
+			rx.Concat(rx.Range(1, 9), rx.Throw(ErrTest)).Pipe(filterLessThan5),
+		},
+		[][]interface{}{
+			{1, 2, 3, 4, 4, 3, 2, 1, Completed},
+			{1, 2, 3, 4, Completed},
+			{1, 2, 3, 4, ErrTest},
+		},
+	)
+}
+
 func TestExclude(t *testing.T) {
 	excludeLessThan5 := operators.Exclude(
 		func(val interface{}, idx int) bool {
