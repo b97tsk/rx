d395b8d7c776d1f26936df98f0a046190e6be066
b97tsk <b97tsk@gmail.com>
2020-08-31 12:20:57 +0800

    Update internal/queue

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,96 +1,113 @@
 package queue
 
-const smallSize = 8
+const smallSize = 16
 
 // Queue represents a single instance of the queue data structure. The zero
 // value for a Queue is an empty queue ready to use.
 type Queue struct {
-	buf    []interface{}
-	head   int
-	tail   int
-	length int
+	buf  []interface{}
+	head []interface{}
 }
 
 // Init initializes or clears the Queue.
 func (q *Queue) Init() {
-	*q = Queue{}
+	q.buf, q.head = nil, nil
 }
 
 // Cap returns the capacity of the internal buffer. If Cap() equals to Len(),
 // new Push(x) causes the internal buffer to grow.
 func (q *Queue) Cap() int {
-	return len(q.buf)
+	return cap(q.buf)
 }
 
 // Len returns the number of elements currently stored in the Queue.
 func (q *Queue) Len() int {
-	return q.length
+	return len(q.head) + len(q.buf)
 }
 
 // Push inserts an element at the end of the Queue.
 func (q *Queue) Push(x interface{}) {
-	if q.length == len(q.buf) { // Grow if full.
-		buf := append(q.buf, x)
+	if ql, qc := q.Len(), q.Cap(); ql == qc { // Grow if full.
+		buf := append(q.buf[:qc], nil)
 		q.setbuf(buf[:cap(buf)])
 	}
-	q.buf[q.tail] = x
-	q.tail = (q.tail + 1) % len(q.buf)
-	q.length++
+	if len(q.head) < cap(q.head) {
+		q.head = append(q.head, x)
+	} else {
+		q.buf = append(q.buf, x)
+	}
 }
 
 // Pop removes and returns the first element. It panics if the Queue is empty.
 func (q *Queue) Pop() interface{} {
-	x := q.buf[q.head]
-	q.buf[q.head] = nil
-	max := len(q.buf)
-	q.head = (q.head + 1) % max
-	q.length--
-	if q.length == max/4 && max > smallSize { // Shrink if sparse.
-		q.setbuf(make([]interface{}, q.length*2))
+	if len(q.head) > 0 {
+		x := q.head[0]
+		q.head[0] = nil
+		q.head = q.head[1:]
+		if cap(q.head) == 0 {
+			q.head = q.buf
+			q.buf = q.buf[:0]
+		}
+		if ql, qc := q.Len(), q.Cap(); ql == qc>>2 && qc > smallSize { // Shrink if sparse.
+			q.setbuf(make([]interface{}, ql<<1))
+		}
+		return x
 	}
-	return x
+	panic("queue: Pop called on empty Queue")
 }
 
 // At returns the i-th element in the Queue. It panics if i is out of range.
 func (q *Queue) At(i int) interface{} {
-	return q.buf[(q.head+i)%len(q.buf)]
+	if i >= 0 {
+		headsize := len(q.head)
+		if i < headsize {
+			return q.head[i]
+		}
+		i -= headsize
+		if i < len(q.buf) {
+			return q.buf[i]
+		}
+	}
+	panic("queue: At called with index out of range")
 }
 
 // Front returns the first element. It panics if the Queue is empty.
 func (q *Queue) Front() interface{} {
-	return q.buf[q.head]
+	if len(q.head) > 0 {
+		return q.head[0]
+	}
+	panic("queue: Front called on empty Queue")
 }
 
 // Back returns the last element. It panics if the Queue is empty.
 func (q *Queue) Back() interface{} {
-	max := len(q.buf)
-	return q.buf[(q.tail+max-1)%max]
+	if n := len(q.buf); n > 0 {
+		return q.buf[n-1]
+	}
+	if n := len(q.head); n > 0 {
+		return q.head[n-1]
+	}
+	panic("queue: Back called on empty Queue")
 }
 
-// Copy copies elements of the Queue into a destination slice. Copy returns
-// the number of elements copied, which will be the minimum of q.Len() and
-// len(dst).
-func (q *Queue) Copy(dst []interface{}) int {
-	if q.head < q.tail {
-		return copy(dst, q.buf[q.head:q.tail])
-	}
-	n := copy(dst, q.buf[q.head:])
-	return n + copy(dst[n:], q.buf[:q.tail])
+// CopyTo copies elements of the Queue into a destination slice. CopyTo
+// returns the number of elements copied, which will be the minimum of
+// q.Len() and len(dst).
+func (q *Queue) CopyTo(dst []interface{}) int {
+	n := copy(dst, q.head)
+	return n + copy(dst[n:], q.buf)
 }
 
 // Clone clones the Queue.
 func (q *Queue) Clone() Queue {
-	var b []interface{}
-	if q.length > 0 {
-		b = make([]interface{}, q.length, len(q.buf))
-		q.Copy(b)
+	var buf []interface{}
+	if q.buf != nil {
+		buf = make([]interface{}, q.Len(), q.Cap())
+		q.CopyTo(buf)
 	}
-	return Queue{b, 0, len(b), len(b)}
+	return Queue{buf[:0], buf}
 }
 
 func (q *Queue) setbuf(buf []interface{}) {
-	q.Copy(buf)
-	q.buf = buf
-	q.head = 0
-	q.tail = q.length
+	q.buf, q.head = buf[:0], buf[:q.CopyTo(buf)]
 }
--- internal/queue/queue_test.go
+++ internal/queue/queue_test.go
@@ -14,58 +14,73 @@ func TestQueue(t *testing.T) {
 		t.FailNow()
 	}
 
-	for _, r := range "ABCDEFGHIJKLM" {
-		r := string(r)
-		q.Push(r)
-		t.Logf("PushBack(%v): Len=%v Cap=%v Front=%v Back=%v", r, q.Len(), q.Cap(), q.Front(), q.Back())
-		if q.Back() != r {
-			t.FailNow()
-		}
-	}
-	for _, r := range "ABCDEFG" {
-		r := string(r)
-		v := q.Pop()
-		t.Logf("PopFront(%v): Len=%v Cap=%v Front=%v Back=%v", v, q.Len(), q.Cap(), q.Front(), q.Back())
-		if v != r {
-			t.FailNow()
+	pushLetters := func(s string) {
+		for _, r := range s {
+			r := string(r)
+			q.Push(r)
+			t.Logf("Push(%v): Len=%v Cap=%v Front=%v Back=%v", r, q.Len(), q.Cap(), q.Front(), q.Back())
+			if q.Back() != r {
+				t.FailNow()
+			}
 		}
 	}
 
-	cloned := q.Clone()
-	for i, r := range "HIJKLM" {
+	popLetters := func(s string) {
+		for _, r := range s {
+			r := string(r)
+			v := q.Pop()
+			t.Logf("Pop(%v): Len=%v Cap=%v Front=%v Back=%v", v, q.Len(), q.Cap(), q.Front(), q.Back())
+			if v != r {
+				t.FailNow()
+			}
+		}
+	}
+
+	pushLetters("ABCDEF")
+	popLetters("ABCD")
+	pushLetters("GHIJKL")
+
+	for i, r := range "EFGHIJKL" {
 		r := string(r)
-		ith := cloned.At(i)
+		ith := q.At(i)
 		if ith != r {
 			t.Logf("At(%v): %v expected, but got %v", i, r, ith)
 			t.FailNow()
 		}
 	}
-	cloned.Init()
-	if cloned.Len() != 0 {
-		t.Logf("Reset(nil): Len=%v Cap=%v", cloned.Len(), cloned.Cap())
+
+	popLetters("EFGH")
+	pushLetters("MNOPQRSTUVWXYZ")
+	popLetters("IJKLMNOPQRSTUVWXY")
+
+	cloned := q.Clone()
+	if cloned.Len() != 1 || cloned.Front() != "Z" {
 		t.FailNow()
 	}
-
-	for _, r := range "NOPQRSTUVWXYZ" {
-		r := string(r)
-		q.Push(r)
-		t.Logf("PushBack(%v): Len=%v Cap=%v Front=%v Back=%v", r, q.Len(), q.Cap(), q.Front(), q.Back())
-		if q.Back() != r {
-			t.FailNow()
-		}
-	}
-	for _, r := range "HIJKLMNOPQRSTUVWXY" {
-		r := string(r)
-		v := q.Pop()
-		t.Logf("PopFront(%v): Len=%v Cap=%v Front=%v Back=%v", v, q.Len(), q.Cap(), q.Front(), q.Back())
-		if v != r {
-			t.FailNow()
-		}
+	cloned.Init()
+	if cloned.Len() != 0 {
+		t.Logf("Init(): Len=%v Cap=%v", cloned.Len(), cloned.Cap())
+		t.FailNow()
 	}
 
 	v := q.Pop()
-	t.Logf("PopFront(%v): Len=%v Cap=%v", v, q.Len(), q.Cap())
+	t.Logf("Pop(%v): Len=%v Cap=%v", v, q.Len(), q.Cap())
 	if v != "Z" || q.Len() != 0 {
 		t.FailNow()
 	}
+
+	shouldPanic(t, func() { q.Pop() }, "Pop didn't panic.")
+	shouldPanic(t, func() { q.Front() }, "Front didn't panic.")
+	shouldPanic(t, func() { q.Back() }, "Back didn't panic.")
+	shouldPanic(t, func() { q.At(0) }, "At didn't panic.")
+}
+
+func shouldPanic(t *testing.T, f func(), msg string) {
+	defer func() {
+		if recover() == nil {
+			t.Log(msg)
+			t.FailNow()
+		}
+	}()
+	f()
 }
