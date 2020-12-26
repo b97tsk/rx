d920507c45b0a25491445c98bd233bbe5ec40e8c
b97tsk <b97tsk@gmail.com>
2020-12-26 23:08:04 +0800

    Update operators/merge_test.go

--- operators/merge_test.go
+++ operators/merge_test.go
@@ -8,7 +8,11 @@ import (
 	"github.com/b97tsk/rx/operators"
 )
 
-func TestMergeAll(t *testing.T) {
+func TestMerge(t *testing.T) {
+	sum := func(seed, val interface{}, idx int) interface{} {
+		return seed.(int) + val.(int)
+	}
+
 	NewTestSuite(t).Case(
 		rx.Just(
 			rx.Just("A", "B").Pipe(AddLatencyToValues(3, 5)),
@@ -18,5 +22,31 @@ func TestMergeAll(t *testing.T) {
 			operators.MergeAll(-1),
 		),
 		"E", "C", "A", "F", "D", "B", Completed,
+	).Case(
+		rx.Range(0, 9).Pipe(
+			operators.MergeMap(
+				func(val interface{}, idx int) rx.Observable {
+					return rx.Just(val)
+				},
+				3,
+			),
+			operators.Reduce(sum),
+		),
+		36, Completed,
+	).Case(
+		rx.Timer(Step(1)).Pipe(
+			operators.MergeMapTo(rx.Just("A"), -1),
+		),
+		"A", Completed,
+	).Case(
+		rx.Empty().Pipe(
+			operators.MergeAll(-1),
+		),
+		Completed,
+	).Case(
+		rx.Throw(ErrTest).Pipe(
+			operators.MergeAll(-1),
+		),
+		ErrTest,
 	).TestAll()
 }
