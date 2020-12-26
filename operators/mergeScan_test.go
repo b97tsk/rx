a5dc157bb1551e6fe93563e2ac277c53b88a8544
b97tsk <b97tsk@gmail.com>
2020-12-26 23:20:08 +0800

    Update operators/mergeScan_test.go

--- operators/mergeScan_test.go
+++ operators/mergeScan_test.go
@@ -9,20 +9,44 @@ import (
 )
 
 func TestMergeScan(t *testing.T) {
+	f := func(acc, val interface{}, idx int) rx.Observable {
+		return val.(rx.Observable)
+	}
+
+	sum := func(seed, val interface{}, idx int) interface{} {
+		return seed.(int) + val.(int)
+	}
+
 	NewTestSuite(t).Case(
 		rx.Just(
 			rx.Just("A", "B").Pipe(AddLatencyToValues(3, 5)),
 			rx.Just("C", "D").Pipe(AddLatencyToValues(2, 4)),
 			rx.Just("E", "F").Pipe(AddLatencyToValues(1, 3)),
 		).Pipe(
-			operators.MergeScan(
-				func(acc, val interface{}, idx int) rx.Observable {
-					return val.(rx.Observable)
-				},
-				nil,
-				-1,
-			),
+			operators.MergeScan(f, nil, -1),
 		),
 		"E", "C", "A", "F", "D", "B", Completed,
+	).Case(
+		rx.Range(0, 9).Pipe(
+			operators.MergeScan(
+				func(acc, val interface{}, idx int) rx.Observable {
+					return rx.Just(val)
+				},
+				nil,
+				3,
+			),
+			operators.Reduce(sum),
+		),
+		36, Completed,
+	).Case(
+		rx.Empty().Pipe(
+			operators.MergeScan(f, nil, -1),
+		),
+		Completed,
+	).Case(
+		rx.Throw(ErrTest).Pipe(
+			operators.MergeScan(f, nil, -1),
+		),
+		ErrTest,
 	).TestAll()
 }
