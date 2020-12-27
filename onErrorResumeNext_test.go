9dddf2d7243cc72d6a38399160b1c65d70c7865d
b97tsk <b97tsk@gmail.com>
2020-12-27 15:53:06 +0800

    Update onErrorResumeNext_test.go

--- onErrorResumeNext_test.go
+++ onErrorResumeNext_test.go
@@ -1,6 +1,7 @@
 package rx_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/b97tsk/rx"
@@ -36,5 +37,20 @@ func TestOnErrorResumeNext(t *testing.T) {
 			rx.Throw(ErrTest),
 		),
 		1, 2, 3, 4, 5, 6, Completed,
+	).Case(
+		rx.OnErrorResumeNext(),
+		Completed,
 	).TestAll()
+
+	ctx, cancel := context.WithTimeout(context.Background(), Step(1))
+	defer cancel()
+
+	rx.OnErrorResumeNext(
+		func(_ context.Context, sink rx.Observer) {
+			_ = rx.Timer(Step(2)).BlockingSubscribe(context.Background(), sink)
+		},
+		func(context.Context, rx.Observer) {
+			t.Fatal("should not happen")
+		},
+	).Subscribe(ctx, rx.Noop)
 }
