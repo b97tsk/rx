35ab8eaca7e3c01a282618a22547fca01f5103a9
b97tsk <b97tsk@gmail.com>
2020-12-27 10:45:25 +0800

    Update operators/mergeSync_test.go

--- operators/mergeSync_test.go
+++ operators/mergeSync_test.go
@@ -1,6 +1,7 @@
 package operators_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/b97tsk/rx"
@@ -8,7 +9,7 @@ import (
 	"github.com/b97tsk/rx/operators"
 )
 
-func TestMergeSyncAll(t *testing.T) {
+func TestMergeSync(t *testing.T) {
 	NewTestSuite(t).Case(
 		rx.Just(
 			rx.Just("A", "B").Pipe(AddLatencyToValues(3, 5)),
@@ -18,5 +19,35 @@ func TestMergeSyncAll(t *testing.T) {
 			operators.MergeSyncAll(1),
 		),
 		"A", "B", "C", "D", "E", "F", Completed,
+	).Case(
+		rx.Timer(Step(1)).Pipe(
+			operators.MergeSyncMapTo(rx.Just("A"), 1),
+		),
+		"A", Completed,
+	).Case(
+		rx.Empty().Pipe(
+			operators.MergeSyncAll(1),
+		),
+		Completed,
+	).Case(
+		rx.Throw(ErrTest).Pipe(
+			operators.MergeSyncAll(1),
+		),
+		ErrTest,
 	).TestAll()
+
+	ctx, cancel := context.WithTimeout(context.Background(), Step(1))
+	defer cancel()
+
+	rx.Just("A", "B", "C", "D", "E").Pipe(
+		operators.MergeSyncMap(
+			func(val interface{}, idx int) rx.Observable {
+				return rx.Just(val).Pipe(DelaySubscription(2))
+			},
+			3,
+		),
+		operators.DoOnComplete(
+			func() { t.Fatal("should not happen") },
+		),
+	).Subscribe(ctx, rx.Noop)
 }
