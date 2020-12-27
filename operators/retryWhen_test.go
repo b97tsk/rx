5ce568008a95966b76eed435e4aa2265555bed44
b97tsk <b97tsk@gmail.com>
2020-12-27 11:17:54 +0800

    Update operators/retryWhen_test.go

--- operators/retryWhen_test.go
+++ operators/retryWhen_test.go
@@ -14,43 +14,55 @@ func TestRetryWhen(t *testing.T) {
 	retryTwice := operators.Take(2)
 
 	NewTestSuite(t).Case(
-		rx.Range(1, 4).Pipe(
+		rx.Just("A", "B", "C").Pipe(
 			operators.RetryWhen(retryNever),
 		),
-		1, 2, 3, Completed,
+		"A", "B", "C", Completed,
 	).Case(
-		rx.Range(1, 4).Pipe(
+		rx.Just("A", "B", "C").Pipe(
 			operators.RetryWhen(retryOnce),
 		),
-		1, 2, 3, Completed,
+		"A", "B", "C", Completed,
 	).Case(
-		rx.Range(1, 4).Pipe(
+		rx.Just("A", "B", "C").Pipe(
 			operators.RetryWhen(retryTwice),
 		),
-		1, 2, 3, Completed,
+		"A", "B", "C", Completed,
 	).Case(
 		rx.Concat(
-			rx.Range(1, 4),
+			rx.Just("A", "B", "C"),
 			rx.Throw(ErrTest),
 		).Pipe(
 			operators.RetryWhen(retryNever),
 		),
-		1, 2, 3, ErrTest,
+		"A", "B", "C", ErrTest,
 	).Case(
 		rx.Concat(
-			rx.Range(1, 4),
+			rx.Just("A", "B", "C"),
 			rx.Throw(ErrTest),
 		).Pipe(
 			operators.RetryWhen(retryOnce),
 		),
-		1, 2, 3, 1, 2, 3, ErrTest,
+		"A", "B", "C", "A", "B", "C", ErrTest,
 	).Case(
 		rx.Concat(
-			rx.Range(1, 4),
+			rx.Just("A", "B", "C"),
 			rx.Throw(ErrTest),
 		).Pipe(
 			operators.RetryWhen(retryTwice),
 		),
-		1, 2, 3, 1, 2, 3, 1, 2, 3, ErrTest,
+		"A", "B", "C", "A", "B", "C", "A", "B", "C", ErrTest,
+	).Case(
+		rx.Concat(
+			rx.Just("A", "B", "C"),
+			rx.Throw(ErrTest),
+		).Pipe(
+			operators.RetryWhen(
+				func(rx.Observable) rx.Observable {
+					return rx.Throw(ErrTest)
+				},
+			),
+		),
+		"A", "B", "C", ErrTest,
 	).TestAll()
 }
