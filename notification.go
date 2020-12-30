678c8f6a43b7fd1cb808720384bf0ae804b4843a
b97tsk <b97tsk@gmail.com>
2020-12-30 21:39:57 +0800

    Reorder Notification struct fields

--- notification.go
+++ notification.go
@@ -2,8 +2,8 @@ package rx
 
 // Notification is the representation of an emission.
 type Notification struct {
-	Value    interface{}
-	Error    error
 	HasValue bool
 	HasError bool
+	Value    interface{}
+	Error    error
 }
