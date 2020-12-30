8e197023b7b2e374b0367f3b08a86f714472a36e
b97tsk <b97tsk@gmail.com>
2020-12-30 22:20:26 +0800

    Update operators/do_test.go

--- operators/do_test.go
+++ operators/do_test.go
@@ -9,227 +9,83 @@ import (
 	"github.com/b97tsk/rx/operators"
 )
 
+func testDo(t *testing.T, op func(func()) rx.Operator, r1, r2, r3, r4 int) {
+	n := 0
+
+	do := op(func() { n++ })
+
+	obs := rx.Observable(
+		func(ctx context.Context, sink rx.Observer) {
+			sink.Next(n)
+			sink.Complete()
+		},
+	)
+
+	NewTestSuite(t).Case(
+		rx.Concat(
+			rx.Empty().Pipe(do),
+			obs,
+		),
+		r1, Completed,
+	).Case(
+		rx.Concat(
+			rx.Just("A").Pipe(do),
+			obs,
+		),
+		"A", r2, Completed,
+	).Case(
+		rx.Concat(
+			rx.Just("A", "B").Pipe(do),
+			obs,
+		),
+		"A", "B", r3, Completed,
+	).Case(
+		rx.Concat(
+			rx.Concat(
+				rx.Just("A", "B"),
+				rx.Throw(ErrTest),
+			).Pipe(do),
+			obs,
+		),
+		"A", "B", ErrTest,
+	).Case(
+		obs,
+		r4, Completed,
+	).TestAll()
+}
+
 func TestDo(t *testing.T) {
-	n := 0
-
-	do := operators.Do(func(rx.Notification) { n++ })
-
-	obs := rx.Observable(
-		func(ctx context.Context, sink rx.Observer) {
-			sink.Next(n)
-			sink.Complete()
-		},
-	)
-
-	NewTestSuite(t).Case(
-		rx.Concat(
-			rx.Empty().Pipe(do),
-			obs,
-		),
-		1, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A").Pipe(do),
-			obs,
-		),
-		"A", 3, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A", "B").Pipe(do),
-			obs,
-		),
-		"A", "B", 6, Completed,
-	).Case(
-		rx.Concat(
-			rx.Concat(
-				rx.Just("A", "B"),
-				rx.Throw(ErrTest),
-			).Pipe(do),
-			obs,
-		),
-		"A", "B", ErrTest,
-	).Case(
-		obs,
-		9, Completed,
-	).TestAll()
-}
-
-func TestDoOnNext(t *testing.T) {
-	n := 0
-
-	do := operators.DoOnNext(func(interface{}) { n++ })
-
-	obs := rx.Observable(
-		func(ctx context.Context, sink rx.Observer) {
-			sink.Next(n)
-			sink.Complete()
-		},
-	)
-
-	NewTestSuite(t).Case(
-		rx.Concat(
-			rx.Empty().Pipe(do),
-			obs,
-		),
-		0, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A").Pipe(do),
-			obs,
-		),
-		"A", 1, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A", "B").Pipe(do),
-			obs,
-		),
-		"A", "B", 3, Completed,
-	).Case(
-		rx.Concat(
-			rx.Concat(
-				rx.Just("A", "B"),
-				rx.Throw(ErrTest),
-			).Pipe(do),
-			obs,
-		),
-		"A", "B", ErrTest,
-	).Case(
-		obs,
-		5, Completed,
-	).TestAll()
-}
-
-func TestDoOnError(t *testing.T) {
-	n := 0
-
-	do := operators.DoOnError(func(error) { n++ })
-
-	obs := rx.Observable(
-		func(ctx context.Context, sink rx.Observer) {
-			sink.Next(n)
-			sink.Complete()
-		},
-	)
-
-	NewTestSuite(t).Case(
-		rx.Concat(
-			rx.Empty().Pipe(do),
-			obs,
-		),
-		0, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A").Pipe(do),
-			obs,
-		),
-		"A", 0, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A", "B").Pipe(do),
-			obs,
-		),
-		"A", "B", 0, Completed,
-	).Case(
-		rx.Concat(
-			rx.Concat(
-				rx.Just("A", "B"),
-				rx.Throw(ErrTest),
-			).Pipe(do),
-			obs,
-		),
-		"A", "B", ErrTest,
-	).Case(
-		obs,
-		1, Completed,
-	).TestAll()
-}
-
-func TestDoOnComplete(t *testing.T) {
-	n := 0
-
-	do := operators.DoOnComplete(func() { n++ })
-
-	obs := rx.Observable(
-		func(ctx context.Context, sink rx.Observer) {
-			sink.Next(n)
-			sink.Complete()
-		},
-	)
-
-	NewTestSuite(t).Case(
-		rx.Concat(
-			rx.Empty().Pipe(do),
-			obs,
-		),
-		1, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A").Pipe(do),
-			obs,
-		),
-		"A", 2, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A", "B").Pipe(do),
-			obs,
-		),
-		"A", "B", 3, Completed,
-	).Case(
-		rx.Concat(
-			rx.Concat(
-				rx.Just("A", "B"),
-				rx.Throw(ErrTest),
-			).Pipe(do),
-			obs,
-		),
-		"A", "B", ErrTest,
-	).Case(
-		obs,
-		3, Completed,
-	).TestAll()
-}
-
-func TestDoOnErrorOrComplete(t *testing.T) {
-	n := 0
-
-	do := operators.DoOnErrorOrComplete(func() { n++ })
-
-	obs := rx.Observable(
-		func(ctx context.Context, sink rx.Observer) {
-			sink.Next(n)
-			sink.Complete()
-		},
-	)
-
-	NewTestSuite(t).Case(
-		rx.Concat(
-			rx.Empty().Pipe(do),
-			obs,
-		),
-		1, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A").Pipe(do),
-			obs,
-		),
-		"A", 2, Completed,
-	).Case(
-		rx.Concat(
-			rx.Just("A", "B").Pipe(do),
-			obs,
-		),
-		"A", "B", 3, Completed,
-	).Case(
-		rx.Concat(
-			rx.Concat(
-				rx.Just("A", "B"),
-				rx.Throw(ErrTest),
-			).Pipe(do),
-			obs,
-		),
-		"A", "B", ErrTest,
-	).Case(
-		obs,
-		4, Completed,
-	).TestAll()
+	t.Run("Do", func(t *testing.T) {
+		testDo(
+			t,
+			func(f func()) rx.Operator {
+				return operators.Do(func(rx.Notification) { f() })
+			},
+			1, 3, 6, 9,
+		)
+	})
+	t.Run("DoOnNext", func(t *testing.T) {
+		testDo(
+			t,
+			func(f func()) rx.Operator {
+				return operators.DoOnNext(func(interface{}) { f() })
+			},
+			0, 1, 3, 5,
+		)
+	})
+	t.Run("DoOnError", func(t *testing.T) {
+		testDo(
+			t,
+			func(f func()) rx.Operator {
+				return operators.DoOnError(func(error) { f() })
+			},
+			0, 0, 0, 1,
+		)
+	})
+	t.Run("DoOnComplete", func(t *testing.T) {
+		testDo(t, operators.DoOnComplete, 1, 2, 3, 3)
+	})
+	t.Run("DoOnErrorOrComplete", func(t *testing.T) {
+		testDo(t, operators.DoOnErrorOrComplete, 1, 2, 3, 4)
+	})
 }
