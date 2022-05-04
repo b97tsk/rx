4f2d73b017ed6347ec3111907da89186327da295
b97tsk <b97tsk@gmail.com>
2022-05-04 08:00:00 +0800

    add Compose and Pipe functions

---
+++ compose.go
@@ -0,0 +1,133 @@
+package rx
+
+// Compose2 creates an Operator that applies 2 Operators to the input
+// Observable when called.
+func Compose2[A, B, C any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+) Operator[A, C] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[C] {
+			return Pipe2(obs, op1, op2)
+		},
+	)
+}
+
+// Compose3 creates an Operator that applies 3 Operators to the input
+// Observable when called.
+func Compose3[A, B, C, D any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+) Operator[A, D] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[D] {
+			return Pipe3(obs, op1, op2, op3)
+		},
+	)
+}
+
+// Compose4 creates an Operator that applies 4 Operators to the input
+// Observable when called.
+func Compose4[A, B, C, D, E any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+) Operator[A, E] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[E] {
+			return Pipe4(obs, op1, op2, op3, op4)
+		},
+	)
+}
+
+// Compose5 creates an Operator that applies 5 Operators to the input
+// Observable when called.
+func Compose5[A, B, C, D, E, F any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+) Operator[A, F] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[F] {
+			return Pipe5(obs, op1, op2, op3, op4, op5)
+		},
+	)
+}
+
+// Compose6 creates an Operator that applies 6 Operators to the input
+// Observable when called.
+func Compose6[A, B, C, D, E, F, G any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+) Operator[A, G] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[G] {
+			return Pipe6(obs, op1, op2, op3, op4, op5, op6)
+		},
+	)
+}
+
+// Compose7 creates an Operator that applies 7 Operators to the input
+// Observable when called.
+func Compose7[A, B, C, D, E, F, G, H any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+) Operator[A, H] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[H] {
+			return Pipe7(obs, op1, op2, op3, op4, op5, op6, op7)
+		},
+	)
+}
+
+// Compose8 creates an Operator that applies 8 Operators to the input
+// Observable when called.
+func Compose8[A, B, C, D, E, F, G, H, I any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+	op8 Operator[H, I],
+) Operator[A, I] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[I] {
+			return Pipe8(obs, op1, op2, op3, op4, op5, op6, op7, op8)
+		},
+	)
+}
+
+// Compose9 creates an Operator that applies 9 Operators to the input
+// Observable when called.
+func Compose9[A, B, C, D, E, F, G, H, I, J any](
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+	op8 Operator[H, I],
+	op9 Operator[I, J],
+) Operator[A, J] {
+	return AsOperator(
+		func(obs Observable[A]) Observable[J] {
+			return Pipe9(obs, op1, op2, op3, op4, op5, op6, op7, op8, op9)
+		},
+	)
+}
---
+++ pipe.go
@@ -0,0 +1,118 @@
+package rx
+
+// Pipe applies an Operator to an Observable and returns the resulting
+// Observable.
+func Pipe[A, B any](
+	obs Observable[A],
+	op Operator[A, B],
+) Observable[B] {
+	return op.Apply(obs)
+}
+
+// Pipe2 applies 2 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe2[A, B, C any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+) Observable[C] {
+	return op2.Apply(op1.Apply(obs))
+}
+
+// Pipe3 applies 3 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe3[A, B, C, D any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+) Observable[D] {
+	return op3.Apply(op2.Apply(op1.Apply(obs)))
+}
+
+// Pipe4 applies 4 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe4[A, B, C, D, E any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+) Observable[E] {
+	return op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs))))
+}
+
+// Pipe5 applies 5 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe5[A, B, C, D, E, F any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+) Observable[F] {
+	return op5.Apply(op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs)))))
+}
+
+// Pipe6 applies 6 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe6[A, B, C, D, E, F, G any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+) Observable[G] {
+	return op6.Apply(op5.Apply(op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs))))))
+}
+
+// Pipe7 applies 7 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe7[A, B, C, D, E, F, G, H any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+) Observable[H] {
+	return op7.Apply(op6.Apply(op5.Apply(op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs)))))))
+}
+
+// Pipe8 applies 8 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe8[A, B, C, D, E, F, G, H, I any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+	op8 Operator[H, I],
+) Observable[I] {
+	return op8.Apply(op7.Apply(op6.Apply(op5.Apply(op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs))))))))
+}
+
+// Pipe9 applies 9 Operators to an Observable and returns the resulting
+// Observable.
+func Pipe9[A, B, C, D, E, F, G, H, I, J any](
+	obs Observable[A],
+	op1 Operator[A, B],
+	op2 Operator[B, C],
+	op3 Operator[C, D],
+	op4 Operator[D, E],
+	op5 Operator[E, F],
+	op6 Operator[F, G],
+	op7 Operator[G, H],
+	op8 Operator[H, I],
+	op9 Operator[I, J],
+) Observable[J] {
+	return op9.Apply(op8.Apply(op7.Apply(op6.Apply(op5.Apply(op4.Apply(op3.Apply(op2.Apply(op1.Apply(obs)))))))))
+}
