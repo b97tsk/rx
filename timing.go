ce5f7264b1b52fea6724c05145b209043e05c7e6
b97tsk <b97tsk@gmail.com>
2022-05-09 08:00:00 +0800

    add Ticker and Timer observables

---
+++ timing.go
@@ -0,0 +1,56 @@
+package rx
+
+import (
+	"context"
+	"time"
+
+	"github.com/b97tsk/rx/internal/timerpool"
+)
+
+// Ticker creates an Observable that emits time.Time values every specified
+// interval of time.
+func Ticker(d time.Duration) Observable[time.Time] {
+	if d <= 0 {
+		panic("d <= 0")
+	}
+
+	return func(ctx context.Context, sink Observer[time.Time]) {
+		tk := time.NewTicker(d)
+
+		go func() {
+			defer tk.Stop()
+
+			done := ctx.Done()
+
+			for {
+				select {
+				case <-done:
+					sink.Error(ctx.Err())
+					return
+				case t := <-tk.C:
+					sink.Next(t)
+				}
+			}
+		}()
+	}
+}
+
+// Timer creates an Observable that emits only a time.Time value after
+// a particular time span has passed.
+func Timer(d time.Duration) Observable[time.Time] {
+	return func(ctx context.Context, sink Observer[time.Time]) {
+		tm := timerpool.Get(d)
+
+		go func() {
+			select {
+			case <-ctx.Done():
+				timerpool.Put(tm)
+				sink.Error(ctx.Err())
+			case t := <-tm.C:
+				timerpool.PutExpired(tm)
+				sink.Next(t)
+				sink.Complete()
+			}
+		}()
+	}
+}
