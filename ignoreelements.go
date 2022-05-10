6e205a98cf6d6af88733e236ebb1114aad5e5123
b97tsk <b97tsk@gmail.com>
2022-05-10 08:00:00 +0800

    add IgnoreElements operator

---
+++ ignoreelements.go
@@ -0,0 +1,25 @@
+package rx
+
+import (
+	"context"
+)
+
+// IgnoreElements ignores all values emitted by the source Observable and
+// only mirrors errors or completions.
+func IgnoreElements[T, R any]() Operator[T, R] {
+	return AsOperator(ignoreElements[T, R])
+}
+
+func ignoreElements[T, R any](source Observable[T]) Observable[R] {
+	return func(ctx context.Context, sink Observer[R]) {
+		source.Subscribe(ctx, func(n Notification[T]) {
+			switch {
+			case n.HasValue:
+			case n.HasError:
+				sink.Error(n.Error)
+			default:
+				sink.Complete()
+			}
+		})
+	}
+}
