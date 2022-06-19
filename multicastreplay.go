ed08198a25229e776d98dc8b4b2fe428042f4c8a
b97tsk <b97tsk@gmail.com>
2022-06-19 08:00:00 +0800

    add MulticastReplay subject

---
+++ multicastreplay.go
@@ -0,0 +1,201 @@
+package rx
+
+import (
+	"context"
+	"runtime"
+	"time"
+
+	"github.com/b97tsk/rx/internal/atomic"
+	"github.com/b97tsk/rx/internal/ctxwatch"
+	"github.com/b97tsk/rx/internal/queue"
+)
+
+// ReplayConfig carries options for MulticastReplay.
+type ReplayConfig struct {
+	BufferSize int
+	WindowTime time.Duration
+}
+
+// MulticastReplay returns a Subject whose Observable part takes care of
+// all Observers that subscribed to it, all of which will receive buffered
+// emissions and new emissions from Subject's Observer part.
+//
+// Subjects are subject to memory leaks. Once you have done using a Subject,
+// you should at least call its Error or Complete method once to avoid that.
+// If you can guarantee that every subscription to the Subject is canceled,
+// then you are fine.
+//
+// Internally, a runtime finalizer is set to call Error(ErrFinalized), but
+// it is not guaranteed to work.
+func MulticastReplay[T any](opts *ReplayConfig) Subject[T] {
+	m := new(multicastReplay[T])
+
+	if opts != nil {
+		m.ReplayConfig = *opts
+	}
+
+	runtime.SetFinalizer(&m, multicastReplayFinalizer[T])
+
+	return Subject[T]{
+		Observable: func(ctx context.Context, sink Observer[T]) {
+			m.subscribe(ctx, sink)
+		},
+		Observer: func(n Notification[T]) {
+			m.sink(n)
+		},
+	}
+}
+
+func multicastReplayFinalizer[T any](m **multicastReplay[T]) {
+	go (*m).sink(Error[T](ErrFinalized))
+}
+
+type multicastReplay[T any] struct {
+	multicast[T]
+	ReplayConfig
+	buffer   queue.Queue[Pair[time.Time, T]]
+	bufferRc *atomic.Uint32
+}
+
+func (m *multicastReplay[T]) bufferForRead() (queue.Queue[Pair[time.Time, T]], *atomic.Uint32) {
+	rc := m.bufferRc
+
+	if rc == nil {
+		rc = new(atomic.Uint32)
+		m.bufferRc = rc
+	}
+
+	rc.Add(1)
+
+	return m.buffer, rc
+}
+
+func (m *multicastReplay[T]) bufferForWrite() *queue.Queue[Pair[time.Time, T]] {
+	rc := m.bufferRc
+
+	if rc != nil && !rc.Equal(0) {
+		m.buffer = m.buffer.Clone()
+		m.bufferRc = nil
+	}
+
+	return &m.buffer
+}
+
+func (m *multicastReplay[T]) trimBuffer(b *queue.Queue[Pair[time.Time, T]]) {
+	if m.WindowTime > 0 {
+		if b == nil {
+			b = m.bufferForWrite()
+		}
+
+		now := time.Now()
+
+		for b.Len() > 0 {
+			if b.Front().Key.After(now) {
+				break
+			}
+
+			b.Pop()
+		}
+	}
+
+	if n := m.BufferSize; n > 0 {
+		if b == nil {
+			b = m.bufferForWrite()
+		}
+
+		for b.Len() > n {
+			b.Pop()
+		}
+	}
+}
+
+func (m *multicastReplay[T]) sink(n Notification[T]) {
+	m.mu.Lock()
+
+	switch {
+	case m.err != nil:
+		m.mu.Unlock()
+
+	case n.HasValue:
+		obs := m.obs.Clone()
+		defer obs.Release()
+
+		var deadline time.Time
+
+		if windowTime := m.WindowTime; windowTime > 0 {
+			deadline = time.Now().Add(windowTime)
+		}
+
+		b := m.bufferForWrite()
+		b.Push(MakePair(deadline, n.Value))
+		m.trimBuffer(b)
+
+		m.mu.Unlock()
+
+		for _, observer := range obs.Observers {
+			observer.Sink(n)
+		}
+
+	default:
+		var obs multiObserver[T]
+		obs, m.obs = m.obs, obs
+
+		m.err = errCompleted
+		if n.HasError {
+			m.err = errOrErrNil(n.Error)
+			m.buffer.Init()
+		}
+
+		m.mu.Unlock()
+
+		for _, observer := range obs.Observers {
+			observer.Sink(n)
+		}
+	}
+}
+
+func (m *multicastReplay[T]) subscribe(ctx context.Context, sink Observer[T]) {
+	m.mu.Lock()
+
+	err := m.err
+	if err == nil {
+		var cancel context.CancelFunc
+
+		ctx, cancel = context.WithCancel(ctx)
+		sink = sink.WithCancel(cancel).Mutex()
+
+		observer := sink
+		m.obs.Add(&observer)
+		ctxwatch.Add(ctx, func(ctx context.Context) {
+			m.mu.Lock()
+			m.obs.Delete(&observer)
+			m.mu.Unlock()
+			observer.Error(ctx.Err())
+		})
+	}
+
+	m.trimBuffer(nil)
+
+	b, rc := m.bufferForRead()
+	defer rc.Sub(1)
+
+	m.mu.Unlock()
+
+	for i, j := 0, b.Len(); i < j; i++ {
+		if err := ctx.Err(); err != nil {
+			sink.Error(err)
+			return
+		}
+
+		sink.Next(b.At(i).Value)
+	}
+
+	if err != nil {
+		if err == errCompleted {
+			sink.Complete()
+			return
+		}
+
+		sink.Error(cleanErrNil(err))
+	}
+}
---
+++ multicastreplay_test.go
@@ -0,0 +1,172 @@
+package rx_test
+
+import (
+	"context"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/b97tsk/rx"
+	. "github.com/b97tsk/rx/internal/rxtest"
+)
+
+func TestMulticastReplay(t *testing.T) {
+	t.Parallel()
+
+	t.Run("BufferSize", func(t *testing.T) {
+		t.Parallel()
+
+		m := rx.MulticastReplay[string](&rx.ReplayConfig{BufferSize: 3})
+
+		subscribeThenComplete := rx.AsObservable(
+			func(ctx context.Context, sink rx.Observer[string]) {
+				sink = sink.Mutex()
+				m.Subscribe(ctx, sink)
+				sink.Complete()
+			},
+		)
+
+		m.Next("A")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", ErrCompleted)
+
+		m.Next("B")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", "B", ErrCompleted)
+
+		m.Next("C")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", "B", "C", ErrCompleted)
+
+		ctx, cancel := context.WithTimeout(context.Background(), Step(2))
+		defer cancel()
+
+		go rx.Pipe(
+			m.Observable,
+			rx.DoOnNext(
+				func(string) {
+					time.Sleep(Step(2))
+				},
+			),
+		).Subscribe(ctx, rx.Noop[string])
+
+		time.Sleep(Step(1))
+
+		m.Next("D")
+
+		time.Sleep(Step(2))
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "B", "C", "D", ErrCompleted)
+
+		m.Error(ErrTest)
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, ErrTest)
+	})
+
+	t.Run("WindowTime", func(t *testing.T) {
+		t.Parallel()
+
+		m := rx.MulticastReplay[string](&rx.ReplayConfig{WindowTime: Step(5)})
+
+		subscribeThenComplete := rx.AsObservable(
+			func(ctx context.Context, sink rx.Observer[string]) {
+				sink = sink.Mutex()
+				m.Subscribe(ctx, sink)
+				sink.Complete()
+			},
+		)
+
+		m.Next("A")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", ErrCompleted)
+
+		time.Sleep(Step(2))
+		m.Next("B")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", "B", ErrCompleted)
+
+		time.Sleep(Step(2))
+		m.Next("C")
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "A", "B", "C", ErrCompleted)
+
+		time.Sleep(Step(2))
+		m.Next("D")
+		m.Complete()
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "B", "C", "D", ErrCompleted)
+
+		time.Sleep(Step(2))
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "C", "D", ErrCompleted)
+
+		time.Sleep(Step(2))
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, "D", ErrCompleted)
+
+		time.Sleep(Step(2))
+
+		NewTestSuite[string](t).Case(subscribeThenComplete, ErrCompleted)
+	})
+
+	t.Run("AfterComplete", func(t *testing.T) {
+		t.Parallel()
+
+		m := rx.MulticastReplay[string](nil)
+
+		m.Complete()
+
+		NewTestSuite[string](t).Case(m.Observable, ErrCompleted)
+
+		m.Error(ErrTest)
+
+		NewTestSuite[string](t).Case(m.Observable, ErrCompleted)
+	})
+
+	t.Run("AfterError", func(t *testing.T) {
+		t.Parallel()
+
+		m := rx.MulticastReplay[string](nil)
+
+		m.Error(ErrTest)
+
+		NewTestSuite[string](t).Case(m.Observable, ErrTest)
+
+		m.Complete()
+
+		NewTestSuite[string](t).Case(m.Observable, ErrTest)
+	})
+
+	t.Run("NilError", func(t *testing.T) {
+		t.Parallel()
+
+		m := rx.MulticastReplay[string](nil)
+
+		m.Error(nil)
+
+		NewTestSuite[string](t).Case(m.Observable, nil)
+	})
+
+	t.Run("Finalizer", func(t *testing.T) {
+		t.Parallel()
+
+		c := make(chan struct{})
+
+		m := rx.MulticastReplay[string](nil)
+		m.Subscribe(context.Background(), func(n rx.Notification[string]) {
+			if n.Error != rx.ErrFinalized {
+				panic("want rx.ErrFinalized, but got something else")
+			}
+
+			close(c)
+		})
+
+		runtime.GC()
+
+		select {
+		case <-c:
+		case <-time.After(5 * time.Second):
+			t.Fatal("timeout waiting for running finalizer")
+		}
+	})
+}
