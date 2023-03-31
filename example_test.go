c0e8bcf0e8a75431b4d98ccc95b5ac653624e656
b97tsk <b97tsk@gmail.com>
2023-03-31 08:00:00 +0800

    add another example

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package rx_test
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/b97tsk/rx"
 )
@@ -38,13 +39,12 @@ func Example() {
 		),
 	)
 
-	// To Subscribe to an Observable, you call its Subscribe method which
-	// takes a context.Context and an Observer as arguments.
+	// To Subscribe to an Observable, you call its Subscribe method, which takes
+	// a context.Context and an Observer as arguments.
 	obs.Subscribe(context.TODO(), rx.Noop[int])
 
-	// Since this example has no other goroutines involved, it must have
-	// already done. You could also use BlockingSubscribe method instead.
-	// It blocks until done.
+	// Since this example has no other goroutines involved, it must have already done.
+	// You can also use BlockingSubscribe method instead. It blocks until done.
 
 	// Output:
 	// 2
@@ -53,4 +53,31 @@ func Example() {
 	// 14
 	// 18
 	// Complete
+}
+
+func Example_blocking() {
+	ctx, cancel := context.WithTimeout(context.Background(), 700*time.Millisecond)
+	defer cancel()
+
+	obs := rx.Pipe4(
+		rx.Concat(
+			rx.Timer(50*time.Millisecond),
+			rx.Ticker(100*time.Millisecond),
+		),
+		rx.Scan(-1, func(i int, _ time.Time) int { return i + 1 }), // 0, 1, 2, 3, ...
+		rx.Scan(0, func(v1, v2 int) int { return v1 + v2 }),        // 0, 1, 3, 6, ...
+		rx.Skip[int](4),
+		rx.OnNext(func(v int) { fmt.Println(v) }), // 10, 15, 21, ...
+	)
+
+	err := obs.BlockingSubscribe(ctx, rx.Noop[int])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Output:
+	// 10
+	// 15
+	// 21
+	// context deadline exceeded
 }
