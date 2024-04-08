259b9c7d83834ed37c8f5f11e2e1031c1c2b36ed
b97tsk <b97tsk@gmail.com>
2024-04-08 08:00:00 +0800

    rename Congest operators to OnBackpressureXXX

--- backpressure.go
+++ backpressure.go
@@ -2,14 +2,73 @@ package rx
 
 import "github.com/b97tsk/rx/internal/queue"
 
-// CongestBlock mirrors the source Observable, buffering emissions
-// if the source emits too fast, and blocking the source if the buffer is
-// full.
-func CongestBlock[T any](capacity int) Operator[T, T] {
+// OnBackpressureBuffer mirrors the source Observable, buffering emissions
+// if the source emits too fast, and terminating the subscription with
+// a notification of ErrBufferOverflow if the buffer is full.
+func OnBackpressureBuffer[T any](capacity int) Operator[T, T] {
 	return Channelize(
 		func(upstream <-chan Notification[T], downstream chan<- Notification[T]) {
 			if capacity < 1 {
-				panic("CongestBlock: capacity < 1")
+				panic("OnBackpressureBuffer: capacity < 1")
+			}
+
+			var buf queue.Queue[Notification[T]]
+
+			var complete bool
+
+			for {
+				var (
+					in   = upstream
+					out  chan<- Notification[T]
+					outv Notification[T]
+				)
+
+				if buf.Len() != 0 {
+					out, outv = downstream, buf.Front()
+				}
+
+				select {
+				case n := <-in:
+					switch n.Kind {
+					case KindNext:
+						if buf.Len() == capacity {
+							downstream <- Error[T](ErrBufferOverflow)
+							return
+						}
+
+						buf.Push(n)
+					case KindError:
+						downstream <- n
+						return
+					case KindComplete:
+						complete = true
+
+						if buf.Len() == 0 {
+							downstream <- n
+							return
+						}
+					}
+				case out <- outv:
+					buf.Pop()
+
+					if complete && buf.Len() == 0 {
+						downstream <- Complete[T]()
+						return
+					}
+				}
+			}
+		},
+	)
+}
+
+// OnBackpressureCongest mirrors the source Observable, buffering emissions
+// if the source emits too fast, and blocking the source if the buffer is
+// full.
+func OnBackpressureCongest[T any](capacity int) Operator[T, T] {
+	return Channelize(
+		func(upstream <-chan Notification[T], downstream chan<- Notification[T]) {
+			if capacity < 1 {
+				panic("OnBackpressureCongest: capacity < 1")
 			}
 
 			var buf queue.Queue[Notification[T]]
@@ -62,14 +121,14 @@ func CongestBlock[T any](capacity int) Operator[T, T] {
 	)
 }
 
-// CongestDropLatest mirrors the source Observable, buffering emissions
+// OnBackpressureDrop mirrors the source Observable, buffering emissions
 // if the source emits too fast, and dropping emissions if the buffer is
 // full.
-func CongestDropLatest[T any](capacity int) Operator[T, T] {
+func OnBackpressureDrop[T any](capacity int) Operator[T, T] {
 	return Channelize(
 		func(upstream <-chan Notification[T], downstream chan<- Notification[T]) {
 			if capacity < 1 {
-				panic("CongestDropLatest: capacity < 1")
+				panic("OnBackpressureDrop: capacity < 1")
 			}
 
 			var buf queue.Queue[Notification[T]]
@@ -118,14 +177,14 @@ func CongestDropLatest[T any](capacity int) Operator[T, T] {
 	)
 }
 
-// CongestDropOldest mirrors the source Observable, buffering emissions
+// OnBackpressureLatest mirrors the source Observable, buffering emissions
 // if the source emits too fast, and dropping oldest emissions from
 // the buffer if it is full.
-func CongestDropOldest[T any](capacity int) Operator[T, T] {
+func OnBackpressureLatest[T any](capacity int) Operator[T, T] {
 	return Channelize(
 		func(upstream <-chan Notification[T], downstream chan<- Notification[T]) {
 			if capacity < 1 {
-				panic("CongestDropOldest: capacity < 1")
+				panic("OnBackpressureLatest: capacity < 1")
 			}
 
 			var buf queue.Queue[Notification[T]]
@@ -149,65 +208,6 @@ func CongestDropOldest[T any](capacity int) Operator[T, T] {
 					case KindNext:
 						if buf.Len() == capacity {
 							buf.Pop()
-						}
-
-						buf.Push(n)
-					case KindError:
-						downstream <- n
-						return
-					case KindComplete:
-						complete = true
-
-						if buf.Len() == 0 {
-							downstream <- n
-							return
-						}
-					}
-				case out <- outv:
-					buf.Pop()
-
-					if complete && buf.Len() == 0 {
-						downstream <- Complete[T]()
-						return
-					}
-				}
-			}
-		},
-	)
-}
-
-// CongestError mirrors the source Observable, buffering emissions
-// if the source emits too fast, and terminating the subscription with
-// a notification of ErrBufferOverflow if the buffer is full.
-func CongestError[T any](capacity int) Operator[T, T] {
-	return Channelize(
-		func(upstream <-chan Notification[T], downstream chan<- Notification[T]) {
-			if capacity < 1 {
-				panic("CongestError: capacity < 1")
-			}
-
-			var buf queue.Queue[Notification[T]]
-
-			var complete bool
-
-			for {
-				var (
-					in   = upstream
-					out  chan<- Notification[T]
-					outv Notification[T]
-				)
-
-				if buf.Len() != 0 {
-					out, outv = downstream, buf.Front()
-				}
-
-				select {
-				case n := <-in:
-					switch n.Kind {
-					case KindNext:
-						if buf.Len() == capacity {
-							downstream <- Error[T](ErrBufferOverflow)
-							return
 						}
 
 						buf.Push(n)
---
+++ backpressure_test.go
@@ -0,0 +1,178 @@
+package rx_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/b97tsk/rx"
+	. "github.com/b97tsk/rx/internal/rxtest"
+)
+
+func TestOnBackpressure(t *testing.T) {
+	t.Parallel()
+
+	onNext := func(v int) {
+		n := 4
+
+		if v == 1 {
+			// The first value arrives at odd step.
+			// Decrease n to make sure follow-ups called at even steps so as to
+			// pass the tests.
+			n--
+		}
+
+		time.Sleep(Step(n))
+	}
+
+	t.Run("Buffer", func(t *testing.T) {
+		t.Parallel()
+
+		NewTestSuite[int](t).Case(
+			rx.Pipe3(
+				rx.Range(1, 8),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureBuffer[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 5, 6, 7, ErrComplete,
+		).Case(
+			rx.Pipe3(
+				rx.Range(1, 11),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureBuffer[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, rx.ErrBufferOverflow,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureBuffer[int](3),
+			),
+			ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Throw[int](ErrTest),
+				rx.OnBackpressureBuffer[int](3),
+			),
+			ErrTest,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureBuffer[int](0),
+			),
+			rx.ErrOops, "OnBackpressureBuffer: capacity < 1",
+		)
+	})
+
+	t.Run("Congest", func(t *testing.T) {
+		t.Parallel()
+
+		NewTestSuite[int](t).Case(
+			rx.Pipe3(
+				rx.Range(1, 8),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureCongest[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 5, 6, 7, ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureCongest[int](3),
+			),
+			ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Throw[int](ErrTest),
+				rx.OnBackpressureCongest[int](3),
+			),
+			ErrTest,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureCongest[int](0),
+			),
+			rx.ErrOops, "OnBackpressureCongest: capacity < 1",
+		)
+	})
+
+	t.Run("Drop", func(t *testing.T) {
+		t.Parallel()
+
+		NewTestSuite[int](t).Case(
+			rx.Pipe3(
+				rx.Range(1, 8),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureDrop[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 5, 6, 7, ErrComplete,
+		).Case(
+			rx.Pipe3(
+				rx.Range(1, 11),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureDrop[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 5, 6, 7, 9, ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureDrop[int](3),
+			),
+			ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Throw[int](ErrTest),
+				rx.OnBackpressureDrop[int](3),
+			),
+			ErrTest,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureDrop[int](0),
+			),
+			rx.ErrOops, "OnBackpressureDrop: capacity < 1",
+		)
+	})
+
+	t.Run("Latest", func(t *testing.T) {
+		t.Parallel()
+
+		NewTestSuite[int](t).Case(
+			rx.Pipe3(
+				rx.Range(1, 8),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureLatest[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 5, 6, 7, ErrComplete,
+		).Case(
+			rx.Pipe3(
+				rx.Range(1, 11),
+				AddLatencyToValues[int](1, 2),
+				rx.OnBackpressureLatest[int](3),
+				rx.DoOnNext(onNext),
+			),
+			1, 2, 3, 4, 6, 8, 9, 10, ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureLatest[int](3),
+			),
+			ErrComplete,
+		).Case(
+			rx.Pipe1(
+				rx.Throw[int](ErrTest),
+				rx.OnBackpressureLatest[int](3),
+			),
+			ErrTest,
+		).Case(
+			rx.Pipe1(
+				rx.Empty[int](),
+				rx.OnBackpressureLatest[int](0),
+			),
+			rx.ErrOops, "OnBackpressureLatest: capacity < 1",
+		)
+	})
+}
